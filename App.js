03bf43b9da08ccd2f44e2789187f7e34a539bd1d
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-07 10:23:21 -0700

    Add functionality

--- App.js
+++ App.js
@@ -9,12 +9,12 @@
             let value = e.target.dataset.num;
             screen.value += value;
         })
-    })
+    });
 
     equal.addEventListener("click", function (e) {
-        if (screen.value === "") {
-            screen.value = "";
-            console.log(equal);
+        if (screen.value === " ") {
+            screen.value = "Print nummber ";
+
         } else {
             let answer = eval(screen.value);
             screen.value = answer;
