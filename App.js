a6776149d077020de465ee49cf65219dc5276359
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-07 13:56:09 -0700

    Add equal

--- App.js
+++ App.js
@@ -3,28 +3,36 @@
     let buttons = document.querySelectorAll(".btn");
     let clear = document.querySelector(".btn-clear");
     let equal = document.querySelector(".btn-equal");
+    let val = document.querySelector(".btn[data-num]");
 
     buttons.forEach(function (button) {
         button.addEventListener("click", function (e) {
             let value = e.target.dataset.num;
+
             screen.value += value;
+            console.log(screen.value);
         })
+
+
+        equal.addEventListener("click", function (e) {
+            if (screen.value === " ") {
+                screen.value = "Print nummber ";
+
+            } else {
+                let answer = eval(screen.value);
+                screen.value = answer;
+            }
+        });
+
+        clear.addEventListener("click", function (e) {
+            screen.value = "";
+
+        });
     });
 
-    equal.addEventListener("click", function (e) {
-        if (screen.value === " ") {
-            screen.value = "Print nummber ";
 
-        } else {
-            let answer = eval(screen.value);
-            screen.value = answer;
-        }
-    });
 
-    clear.addEventListener("click", function (e) {
-        screen.value = "";
 
-    });
 
 
 })();
